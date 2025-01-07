9f5e441de30f4692e9499b4abc11761f31d2ca48
Emílio Silva <emiliosarges@gmail.com>
2025-01-07 19:03:15 -0300

    teste

--- cookin-up/src/App.vue
+++ cookin-up/src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Meu primeiro projeto Vue!</h1>
+  <header class="banner">
+    <div class="apresentacao">
+      <img src="./assets/images/logo.svg" alt="Logo do Cookin' Up" class="logo">
+
+
+      <p class="cabecalho-lh frase-cabecalho">
+        <span class="texto-verde">Um banquete de ideias para</span>despertar o chef que há em você!
+      </p>
+
+      <p class="subtitulo-lg">
+        Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+      </p>
+    </div>
+
+    <img src="./assets/images/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e apoiando um celular entre seu rosto e ombro" class="foto-banner">
+  </header>
 </template>
+
