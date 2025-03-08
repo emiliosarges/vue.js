81ee659bfdc03e841392af320b6ad57694d0739e
Emílio Silva <emiliosarges@gmail.com>
2025-03-08 01:52:54 -0300

    primeira-etapa
    v-for / v-if v-else/ v-bind :key / requisição http / funçoes data() , created() - método ciclo de vida

---
+++ cookin-up/src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+//Quando propriedade e valor são igual, podemos deixar somente o nome da propriedade em vez de: { Banner: Banner }
+export default {
+  components: { Banner, ConteudoPrincipal } 
+}
+
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+</template>
+
+<style scoped>
+
+</style>
